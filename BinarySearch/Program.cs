fbaf7bc1729a7a9c4e3328d38e27ac56af29c765
Fabio Borges <fabioborges.ti@outlook.com>
2022-08-22 17:25:08 -0300

    init

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var vetor = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            var buscado = 11;
+            var inicio = 0;
+            var fim = vetor.Length - 1;
+            var indiceBuscado = -1;
+
+            while (inicio <= fim)
+            {
+                var meio = (int)((inicio + fim) / 2);
+
+                if (buscado == vetor[meio])
+                {
+                    indiceBuscado = meio;
+                    break;
+                }
+                else if (buscado > vetor[meio])
+                {
+                    inicio = meio + 1;
+                }
+                else
+                {
+                    fim = meio - 1;
+                }
+            }
+
+            if (indiceBuscado == -1)
+            {
+                Console.WriteLine("Elemento não localizado");
+            }
+            else
+            {
+                Console.WriteLine($"O elemento de valor [{buscado}] encontra-se no indice {indiceBuscado}");
+            }
+        }
+    }
+}
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var vetor = new int[] { 4, 1, 5, 3, 2, 9, 7, 6, 8 };
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                var menor = i;
+
+                for (int j = i + 1; j < vetor.Length; j++)
+                {
+                    if (vetor[j] < vetor[menor])
+                    {
+                        menor = j;
+                    }
+                }
+
+                if (menor != i)
+                {
+                    int v = vetor[i];
+                    int aux = v;
+
+                    vetor[i] = vetor[menor];
+                    vetor[menor] = aux;
+                }
+            }
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine(vetor[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
